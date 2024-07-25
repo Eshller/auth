e7f62a2ba7c4dffb3787d4e291f554193e53b245
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-25 16:52:35 +0530

    added veriffication fields to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,3 +1,4 @@
+const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true }, // New email field
   firstName: { type: String, required: false },
   lastName: { type: String, required: false },
+  emailVerified: {type:Boolean, required:true, default:false},
+  aadhaarVerifeid: {type:Boolean, required:true, default:false},
 });
 
 const User = mongoose.model('User', userSchema);
