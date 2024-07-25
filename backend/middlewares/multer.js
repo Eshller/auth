73e1c07a89f33da1c325a56e030a3b68a1a7993c
Dipayan Das <d.dipayan2002@gmail.com>
2024-07-25 16:44:03 +0530

    added file validation to backend

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,5 +1,7 @@
 const multer = require('multer');
-
+const path = require('path');
+//destination and file name type 
+//TODO: to be changed in production
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "uploads/");
@@ -10,5 +12,24 @@ const fileStorageEngine = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: fileStorageEngine, limits: { fileSize: 100000000 } }).single('aadhaar')
+// Check file type
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images only! (jpeg, jpg, png, pdf)');
+    }
+}
+
+const upload = multer({
+    storage: fileStorageEngine,
+    limits: { fileSize: 100000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('aadhaar')
 module.exports = { upload }
